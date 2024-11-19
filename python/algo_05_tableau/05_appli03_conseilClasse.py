0f4de4436b7dac6d01c9c0efcd430e54b116106c
maristudy <mariannick.study@gmail.com>
2024-11-19 18:39:29 +0100

    cours du 20 novembre

--- python/algo_05_tableau/05_appli03_conseilClasse.py
+++ python/algo_05_tableau/05_appli03_conseilClasse.py
@@ -1,6 +1,200 @@
+"""
 https://docs.python.org/fr/3/howto/sorting.html
 Un tri ascendant simple est très facile : il suffit d'appeler la fonction sorted(). Elle renvoie une nouvelle liste triée :
 >>>
 
 sorted([5, 2, 3, 1, 4])
 [1, 2, 3, 4, 5]
+"""
+
+
+
+"""
+#question 1 : saisir 30 notes
+Nom : saisie des 30 notes
+variables numériques : i, tabNotes(30)
+
+DEBUT
+    Pour i allant de 0 à 29
+        Saisir tabNotes[i]
+    Fin Pour    
+FIN
+
+
+#question 2 : saisir 30 notes, calcul et affiche la moyenne
+Nom : saisie des 30 notes
+variables numériques : i, tabNotes(30), moyenne, somme
+
+DEBUT
+    somme <- 0
+    Pour i allant de 0 à 29
+        Saisir tabNotes[i]
+        somme = somme + tabNotes[i]
+    Fin Pour
+    moyenne <- somme / 30
+    Afficher moyenne
+FIN
+
+
+
+#question 3 : saisir 30 notes, min et max
+Nom : saisie des 30 notes, calcul du min et du max
+variables numériques : i, tabNotes(30), min, max
+
+DEBUT
+    max <- 0
+    min <- 20
+    Pour i allant de 0 à 29
+        Saisir tabNotes[i]
+        Si tabNotes[i] > max alors
+            max <- tabNotes[i]
+        Fin Si
+        Si tabNotes[i] < min alors
+            min <- tabNotes[i]
+        Fin Si
+    Fin Pour
+    Afficher <<min = >>,min,<< max = >>,max
+FIN
+
+
+
+
+#question 4 : saisir 30 notes, min, max, etendue ( programme )
+Nom : saisie des 30 notes, calcul du min et du max
+variables numériques : i, tabNotes(30), min, max, etendue
+
+DEBUT
+    max <- 0
+    min <- 20
+    Pour i allant de 0 à 29
+        Saisir tabNotes[i]
+        Si tabNotes[i] > max alors
+            max <- tabNotes[i]
+        Fin Si
+        Si tabNotes[i] < min alors
+            min <- tabNotes[i]
+        Fin Si
+    Fin Pour
+    etendue <- max - min
+    Afficher <<etendue = >>,etendue
+FIN
+"""
+"""
+max=0
+min=20
+tabNotes=[0]*30
+for i in range(0,30):
+    tabNotes[i]=float(input("notes : "))
+    if tabNotes[i] > max:
+        max = tabNotes[i]
+    if tabNotes[i] < min:
+        min = tabNotes[i]
+print("etendue = ",(max-min))
+
+"""
+"""
+#question 5 : saisir 30 notes, notes sous 10 ( programme )
+Nom : saisie des 30 notes, calcul du min et du max
+variables numériques : i, tabNotes(30), noteSous10
+
+DEBUT
+    noteSous10 <- 0
+    Pour i allant de 0 à 29
+        Saisir tabNotes[i]
+        Si tabNotes[i] < 10 alors
+            noteSous10 <- noteSous10 + 1
+        Fin Si
+    Fin Pour
+    Afficher <<nombre de notes sous 10 = >>,noteSous10
+FIN
+"""
+"""
+noteSous10=0
+tabNotes=[0]*30
+for i in range(0,30):
+    tabNotes[i]=float(input("notes : "))
+    if tabNotes[i] < 10:
+        noteSous10 = noteSous10 + 1
+print("nombre de notes sous 10 = ",noteSous10)
+
+
+"""
+"""
+#question 6 : saisir 30 notes, mediane ( programme )
+Nom : saisie des 30 notes, calcul du min et du max
+variables numériques : i, tabNotes(30), mediane
+
+DEBUT
+    noteSous10 <- 0
+    Pour i allant de 0 à 29
+        Saisir tabNotes[i]
+    Fin Pour
+    Trier(tabNotes,croissant)
+    mediane <- (tabNotes(14)+tabNotes(15))/2
+    Afficher <<mediane = >>,mediane
+FIN
+"""
+"""
+# https://docs.python.org/fr/3/howto/sorting.html
+
+tabNotes=[0]*30
+for i in range(0,30):
+    tabNotes[i]=float(input("notes : "))
+sorted(tabNotes)
+mediane = (tabNotes[14]+tabNotes[15])/2
+print("mediane = ",mediane)
+
+
+"""
+"""
+#question 7 : saisir 30 notes, moyenne elaguee ( programme )
+Nom : saisie des 30 notes, calcul du min, du max et de la moyenne elaguee
+variables numériques : i, tabNotes(30), min, max, somme, nbNotes
+
+DEBUT
+    max <- 0
+    min <- 20
+    somme <- 0
+    nbNotes<- 0
+    Pour i allant de 0 à 29
+        Saisir tabNotes[i]
+        Si tabNotes[i] > max alors
+            max <- tabNotes[i]
+        Fin Si
+        Si tabNotes[i] < min alors
+            min <- tabNotes[i]
+        Fin Si
+    Fin Pour
+    Pour i allant de 0 à 29
+        Si tabNotes[i] != min et tabNotes[i] != max alors
+            somme <- somme + tabNotes[i]
+            nbNotes <= nbNotes + 1
+        Fin Si
+    Fin Pour
+    Afficher <<moyenne elaguee = >>,somme / nbNotes
+FIN
+"""
+"""
+
+max=0
+min=20
+somme=0
+nbNotes=0
+tabNotes=[0]*30
+for i in range(0,30):
+    tabNotes[i]=float(input("notes : "))
+    if tabNotes[i] > max:
+        max = tabNotes[i]
+    if tabNotes[i] < min:
+        min = tabNotes[i]
+for i in range(0,30):
+    if tabNotes[i]!= min and tabNotes[i]!=max:
+        somme=somme+tabNotes[i]
+        nbNotes=nbNotes+1
+print("moyenne elaguee : ",(somme / nbNotes))
+
+"""
+
+
+
+
--- python/algo_05_tableau/05_appli04_produit.py
+++ python/algo_05_tableau/05_appli04_produit.py
@@ -1,64 +1,85 @@
-Fonction AFoisB(A, B, m, p, n)
-   Pour i <- 1 à m
-      Pour j <- 1 à n
-         C[i,j] <- 0
-         Pour k <- 1 à p
-            C[i,j] <- C[i,j] + A[i,k] * B[k,j]
-         Fin Pour
-      Fin Pour
-   Fin Pour
-   Retourner C
-Fin Fonction
+"""
 
-A = Matrice de taille m X p
-B = Matrice de taille p X n
+Nom : multiplication de matrices
+variables numériques : nbLigneA, nbColonneA, nbLigneB, nbColonneB, tabloA, tabloB, tabloC, i, j, k
+
+DEBUT
+    Afficher <<quel est le nombre de ligne de la premiere matrice ? >>
+    Saisir nbLigneA
+    Afficher <<quel est le nombre de colonne de la premiere matrice ? >>
+    Saisir nbColonneA
+    Afficher <<quel est le nombre de ligne de la seconde matrice ? >>
+    Saisir nbLigneB
+    Afficher <<quel est le nombre de colonne de la seconde matrice ? >>
+    Saisir nbColonneB
+    Si nbColonneA=nbLigneB alors:
+        Afficher <<Nous allons calculer le produit des deux matrices>>
+        Afficher <<Saisie de la premiere matrice>>
+        
+        Pour i allant de 0 à nbLigneA-1
+            Pour j allant de 0 à nbColonneA-1
+                Saisir tabloA[i][j]
+            Fin Pour
+        Fin Pour
+        
+        Afficher <<Saisie de la seconde matrice>>
+        
+        Pour i allant de 0 à nbLigneB-1
+            Pour j allant de 0 à nbColonneB-1
+                Saisir tabloB[i][j]
+            Fin Pour
+        Fin Pour                 
+        
+        Pour i allant de 0 à nbLigneA-1 
+            Pour j allant de 0 à nbColonneB-1
+                tabloC[i][j]=0 #initialisation
+                Pour k allant de 0 à nbColonneA-1 #calcul
+                    tabloC[i][j]=tabloC[i][j]+(tabloA[i][k]*tabloB[k][j])
+                Fin Pour
+            Fin Pour
+        Fin Pour
+        
+        Afficher tabloC
+    Sinon :
+        Afficher <<multiplication impossible>>
+    Fin Si
+FIN
 
 
-**********************
+"""
+"""
 
-Principe : Le produit matriciel s'en d duit : le produit de la matrice A (n × m) par la matrice B (m × p) est la matrice C (n × p) telle que l'élément Cij est égal au produit scalaire de la ligne i de la matrice A par la colonne j de la matrice B.
 
- 
+nbLigneA=int(input("quel est le nombre de ligne de la premiere matrice ? "))
+nbColonneA=int(input("quel est le nombre de colonne de la premiere matrice ? "))
+nbLigneB=int(input("quel est le nombre de ligne de la seconde matrice ? "))
+nbColonneB=int(input("quel est le nombre de colonne de la seconde matrice ? "))
+tabloA=[0]*nbLigneA
+tabloB=[0]*nbLigneB
+tabloC=[0]*nbLigneA
+for i in range (0,nbLigneA):
+    tabloA[i]=[0]*nbColonneA
+for i in range (0,nbLigneB):
+    tabloB[i]=[0]*nbColonneB
+for i in range (0,nbLigneA):
+    tabloC[i]=[0]*nbColonneB    
+if nbColonneA==nbLigneB:
+    print("Nous allons calculer le produit des deux matrices")
+    print("Saisie de la premiere matrice")
+    for i in range (0,nbLigneA):
+        for j in range (0,nbColonneA):
+            tabloA[i][j]=float(input())
+    print ("Saisie de la seconde matrice")
+    for i in range (0,nbLigneB):
+        for j in range (0,nbColonneB):
+            tabloB[i][j]=float(input())
+    for i in range (0,nbLigneA):
+        for j in range (0,nbColonneB):
+            tabloC[i][j]=0
+            for k in range (0,nbColonneA):
+                tabloC[i][j]=tabloC[i][j]+(tabloA[i][k]*tabloB[k][j])
+    print("c=",tabloC)
+else :
+    print("multiplication impossible")
 
-CORRECTION
 
-Algorithme      ProduitMatriciel;
-
-const N = 4 ;
-
-var      i,j,k: entier;
-
-           A, B, C : tableau[1..N] de entier;
-Début
-
-ecrire('Entrez les elements de votre premiere matrice ') ;
-Pour i de 1 à N faire
-Pour j de 1 à N faire
-Lire ( A[i,j] );
-Fin Pour
-Fin Pour
-
-ecrire('Entrez les elements de votre deuxieme matrice ') ;
-Pour i de 1 à N faire
-Pour j de 1 à N faire
-Lire ( B[i,j] );
-Fin Pour
-Fin Pour
-
- Pour i de 1 a N Faire
-Pour j de 1 a N Faire
-C[i,j] <- 0
-Pour k de 1 a n Faire
-C[i,j] <- C[i,j] + A[i,k] * B[k,j]
-Fpour
-Fpour
-Fpour
-
-ecrire('Affichage de la matrice ') ;
-Pour i de 1 à N faire
-Pour j de 1 à N faire
-ECRIRE( C[i,j] );
-Fin Pour
-Fin Pour
-
-Fin.
