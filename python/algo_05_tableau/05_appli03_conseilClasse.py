d563cfd6b96bff7f2969e8e2da2fef2d4ff6963b
maristudy <mariannick.study@gmail.com>
2024-12-03 18:01:32 +0100

    previsionnel

--- python/algo_05_tableau/05_appli03_conseilClasse.py
+++ python/algo_05_tableau/05_appli03_conseilClasse.py
@@ -1,11 +1,21 @@
 """
 https://docs.python.org/fr/3/howto/sorting.html
-Un tri ascendant simple est très facile : il suffit d'appeler la fonction sorted(). Elle renvoie une nouvelle liste triée :
->>>
-
-sorted([5, 2, 3, 1, 4])
+Un tri ascendant simple est très facile : il suffit d'appeler la fonction sorted().
+Elle renvoie une nouvelle liste triée :
+>>>sorted([5, 2, 3, 1, 4])
 [1, 2, 3, 4, 5]
 """
+
+
+
+
+
+
+
+
+
+
+
 
 
 
